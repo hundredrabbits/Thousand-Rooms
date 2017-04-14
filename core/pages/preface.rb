b3c800f0be9defe2cb2fb5d299235c837b5e18dd
Devine Lu Linvega <aliceffekt@gmail.com>
2017-04-14 10:16:25 -1000

    Began Preface

--- core/pages/preface.rb
+++ core/pages/preface.rb
@@ -11,11 +11,14 @@ class PrefacePage
     
     @type = "preface"
     
+    @style = {}
+    @style["p"] = "padding:40px; font-size:16px; line-height:20px"
+
   end
 
   def content
 
-    return "<p></p>"
+    return "<p>You wake up in a room, a wooden desk with a piece of blank paper and pencil lies in front of you. The walls of the room are white of an unbreakable matter, the room is about 4 square meters. Inside the furniture, pages with notes written with your handwritting, you have no recollection.</p>"
     
   end
   
