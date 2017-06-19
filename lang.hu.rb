c79a8443adde0a71229a5faeebc17a38fc9f0866
Devine Lu Linvega <aliceffekt@gmail.com>
2017-06-19 09:10:56 -1000

    AR + EL _ HU

---
+++ lang.hu.rb
@@ -0,0 +1,61 @@
+# Author  : Devine Lu Linvega
+# Contact : @neauoire(Twitter)
+
+$lang_hu = {
+  :title => "Ezer Szoba",
+  :title_page => [
+    "Ezer Szoba",
+    "Macska, Bagoly, Denev&eacute;r&nbsp;&&nbsp;R&oacute;ka", "Illusztr&aacute;lta Rekka Bellum<br/>&Iacute;rta Devine Lu Linvega", 
+    "Els&#337; Kiad&aacute;s"
+  ],
+  :preface => "Egy szob&aacute;ban &eacute;bredsz, t&ouml;rhetetlen falakkal k&ouml;rbev&eacute;ve. Egy &iacute;r&oacute;asztalon egy ceruza, &eacute;s egy &uuml;res pap&iacute;rlap van.<br/>Az &iacute;r&oacute;asztalban jegyzetek vannak a te k&eacute;z&iacute;r&aacute;soddal, de nem eml&eacute;kszel r&aacute;juk.",
+  :lesson1 => [
+    "Els&#337; Lecke",
+    "Macska &eacute;bredezik.",
+    "Bagoly egy pap&iacute;rlapot tal&aacute;l.",
+    "Denev&eacute;r egy ceruz&aacute;t tal&aacute;l.",
+    "R&oacute;ka kinyitja az &iacute;r&oacute;asztalt."
+  ],
+  :lesson2 => [
+    "M&aacute;sodik Lecke",
+    "Macska a falat kaparja.",
+    "Bagoly &uuml;zenetet hagy az &iacute;r&oacute;asztalban.",
+    "Denev&eacute;r egy m&aacute;sodik ceruz&aacute;t tal&aacute;l.",
+    "R&oacute;ka sz&eacute;tszereli az &iacute;r&oacute;asztalt."
+  ],
+  :lesson3 => [
+    "Harmadik Lecke",
+    "Macska sz&eacute;tmarcangolja a ceruz&aacute;t &eacute;s a pap&iacute;rlapot.",
+    "Bagoly jegyzeteket tal&aacute;l az &iacute;r&oacute;asztalban.",
+    "Denev&eacute;r sok ceruz&aacute;t tal&aacute;l az &iacute;r&oacute;asztalban.",
+    "R&oacute;ka rosszul szereli &ouml;ssze az &iacute;r&oacute;asztalt."
+  ],
+  :lesson4 =>[
+    "Negyedik Lecke",
+    "Macska belem&aacute;szik az &iacute;r&oacute;asztalba.",
+    "Bagoly &eacute;rtelmet keres.",
+    "Denev&eacute;r feladja.",
+    "R&oacute;k&aacute;nak l&aacute;togat&oacute;ja van."
+  ],
+  :lesson5 =>[
+    "&Ouml;t&ouml;dik Lecke",
+    "Macska kisz&aacute;ll az &iacute;r&oacute;asztalb&oacute;l.",
+    "Bagoly meg&eacute;rti a szob&aacute;t.",
+    "Denev&eacute;r fel&eacute;bred.",
+    "R&oacute;k&aacute;nak m&aacute;sodik l&aacute;togat&oacute;ja van."
+  ],
+  :lesson6 => [
+    "Hatodik Lecke",
+    "Macska t&ouml;nkreteszi az &iacute;r&oacute;asztalt.",
+    "Bagoly egy firk&aacute;t tal&aacute;l.",
+    "Denev&eacute;r egy ceruz&aacute;t tal&aacute;l.",
+    "R&oacute;k&aacute;nak t&uacute;l sok l&aacute;togat&oacute;ja van."
+  ],
+  :lesson7 => [
+    "Utols&oacute; Lecke"
+    "Te mit tenn&eacute;l?",
+  ],
+  :thanks => [
+    "K&ouml;sz&ouml;net"
+  ]
+}
