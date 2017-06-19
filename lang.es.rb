46086817109ef9839590273b3023ba1cbb92892f
Devine Lu Linvega <aliceffekt@gmail.com>
2017-06-19 12:28:51 -1000

    Added :es

---
+++ lang.es.rb
@@ -0,0 +1,60 @@
+# Contact : @neauoire
+
+$lang_en = {
+  :translator => "Fáyer (Antonio Uribe)",
+  :language => "Español",
+  :title => "Mil Habitaciones",
+  :title_page => [
+    "Mil Habitaciones",
+    "Gato, Búho, Murciélago y Zorro","Ilustrado por Rekka Bellum<br />Escrito por Devine Lu Linvega",
+    "Primera Edición"
+  ],
+  :preface => "Despiertas en una habitación, rodeado de paredes irrompibles. Sobre el escritorio se encuentra un lápiz y una hoja blanca de papel.<br> Dentro del escritorio hay notas con tu escritura, no tienes recuerdos de eso.",
+  :lesson1 => [
+    "Lección 1",
+    "Gato despierta.",
+    "Búho encuentra un pedazo de papel.",
+    "Murciélago encuentra un lápiz.",
+    "Zorro abre el escritorio."
+  ],
+  :lesson2 => [
+    "Lección 2",
+    "Gato rasguña las paredes.",
+    "Búho deja un mensaje en el escritorio.",
+    "Murciélago encuentra un segundo lápiz.",
+    "Zorro desarma el escritorio."
+  ],
+  :lesson3 => [
+    "Lección 3",
+    "Gato destruye el lápiz y el papel.",
+    "Búho encuentra notas en el escritorio.",
+    "Murciélago encuentra el escritorio lleno de lápices.",
+    "Zorro construye el escritorio de adentro hacia afuera."
+  ],
+  :lesson4 =>[
+    "Lección 4",
+    "Gato entra al escritorio.",
+    "Búho trata de entender.",
+    "Murciélago no encuentra salida.",
+    "Zorro tiene un visitante."
+  ],
+  :lesson5 =>[
+    "Lección 5",
+    "Gato sale del escritorio.",
+    "Búho entiende la habitación.",
+    "Murciélago despierta.",
+    "Zorro tiene un segundo visitante."
+  ],
+  :lesson6 => [
+    "Lección 6",
+    "Gato destruye el escritorio.",
+    "Búho encuentra un garabato.",
+    "Murciélago encuentra un lápiz.",
+    "Zorro tiene demasiados visitantes."
+  ],
+  :lesson7 => [
+    "Última Lección",
+    "¿Qué harías tú?"
+  ],
+  :thanks => "Gracias"
+}
