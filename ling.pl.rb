79474231e363373d007f893ab6bff560bfe11cac
Tymoteusz Masiakowski <hiddencrest@gmail.com>
2017-06-22 07:50:08 +0200

    Created ling.pl.rb

---
+++ ling.pl.rb
@@ -0,0 +1,60 @@
+# Contact : @mothnode
+
+$lang_en = {
+  :translator => "Tymoteusz Masiakowski",
+  :language => "Polish",
+  :title => "Tysiąc Pokojów",
+  :title_page => [
+    "Tysiąc Pokojów",
+    "Kot, Sowa, Nietoperz i Lis","Ilustrowała Rekka Bellum<br />Napisał Devine Lu Linvega",
+    "Pierwsza Edycja"
+  ],
+  :preface => "Budzisz się w pokoju otoczonym ścianami nie do sforsowania. Na biurku leży ołówek i pusta kartka papieru.<br/> W środku znajdują się notatki z Twoim pismem, ale nie pamiętasz o czym traktują.",
+  :lesson1 => [
+    "Lekcja 1",
+    "Kot się budzi.",
+    "Sowa znajduje kawałek papieru.",
+    "Nietoperz znajduje ołówek.",
+    "Lis otwiera biurko."
+  ],
+  :lesson2 => [
+    "Lekcja 2",
+    "Kot drapie ściany.",
+    "Sowa zostawia wiadomość w biurku.",
+    "Nietoperz znajduje drugi ołówek.",
+    "Lis rozbiera biurko na części."
+  ],
+  :lesson3 => [
+    "Lekcja 3",
+    "Kot niszczy ołówek i kawałek papieru.",
+    "Sowa znajduje notatki w biurku.",
+    "Nietoperz odkrywa, że biurko jest wypełnione ołówkami.",
+    "Lis z precyzją konstruuje biurko."
+  ],
+  :lesson4 =>[
+    "Lekcja 4",
+    "Kot wchodzi do biurka.",
+    "Sowa próbuje zrozumieć.",
+    "Nietoperz widzi brak szansy na ucieczkę.",
+    "Lis ma gościa."
+  ],
+  :lesson5 =>[
+    "Lekcja 5",
+    "Kot wchodzi z biurka.",
+    "Sowa rozumie już pokój.",
+    "Nietoperz się budzi.",
+    "Lis ma drugiego gościa."
+  ],
+  :lesson6 => [
+    "Lekcja 6",
+    "Kot niszczy biurko.",
+    "Sowa znajduje bazgroły.",
+    "Nietoperz znajduje ołówek.",
+    "Lis ma zbyt wielu gości."
+  ],
+  :lesson7 => [
+    "Ostatnia Lekcja",
+    "Co Ty byś zrobił?"
+  ],
+  :thanks => "Dziękujemy"
+}
