0a7230c1e46217bffc7b70640a93a66ce1548a26
tomasy stecko <tomastecko@gmail.com>
2018-06-02 16:41:26 +0200

    translation fix
    typos, rewording, gender neutral language

--- lang.pl.rb
+++ lang.pl.rb
@@ -1,60 +1,60 @@
 # Contact : @mothnode
 
 $lang_pl = {
-  :translator => "Tymoteusz Masiakowski",
+  :translator => "Tymoteusz Masiakowski & Tomasz Stecko",
   :language => "Polish",
   :title => "Tysiąc Pokojów",
   :title_page => [
     "Tysiąc Pokojów",
     "Kot, Sowa, Nietoperz i Lis","Ilustrowała Rekka Bellum<br />Napisał Devine Lu Linvega",
-    "Pierwsza Edycja"
+    "Pierwsze wydanie"
   ],
-  :preface => "Budzisz się w pokoju otoczonym ścianami nie do sforsowania. Na biurku leży ołówek i pusta kartka papieru.<br/> W środku znajdują się notatki z Twoim pismem, ale nie pamiętasz o czym traktują.",
+  :preface => "Budzisz się w pokoju otoczonym ścianami nie do sforsowania. Na biurku leży ołówek i czysta kartka papieru.<br/> W środku znajdują się notatki z Twoim pismem, których wcale sobie nie przypominasz.",
   :lesson1 => [
-    "Lekcja 1",
-    "Kot się budzi.",
-    "Sowa znajduje kawałek papieru.",
+    "Lekcja pierwsza",
+    "Kot budzi się.",
+    "Sowa znajduje kartkę papieru.",
     "Nietoperz znajduje ołówek.",
     "Lis otwiera biurko."
   ],
   :lesson2 => [
-    "Lekcja 2",
-    "Kot drapie ściany.",
+    "Lekcja druga",
+    "Kot drapie ściany pazurami.",
     "Sowa zostawia wiadomość w biurku.",
     "Nietoperz znajduje drugi ołówek.",
     "Lis rozbiera biurko na części."
   ],
   :lesson3 => [
-    "Lekcja 3",
-    "Kot niszczy ołówek i kawałek papieru.",
-    "Sowa znajduje notatki w biurku.",
+    "Lekcja trzecia",
+    "Kot niszczy ołówek i kartke papieru.",
+    "Sowa znajduje w biurku notatki.",
     "Nietoperz odkrywa, że biurko jest wypełnione ołówkami.",
-    "Lis z precyzją konstruuje biurko."
+    "Lis buduje biurko na odwrót."
   ],
   :lesson4 =>[
-    "Lekcja 4",
-    "Kot wchodzi do biurka.",
+    "Lekcja czwarta",
+    "Kot włazi do biurka.",
     "Sowa próbuje zrozumieć.",
-    "Nietoperz widzi brak szansy na ucieczkę.",
+    "Nietoperz nie widzi szansy na ucieczkę.",
     "Lis ma gościa."
   ],
   :lesson5 =>[
-    "Lekcja 5",
-    "Kot wchodzi z biurka.",
+    "Lekcja piąta",
+    "Kot wyłazi z biurka.",
     "Sowa rozumie już pokój.",
-    "Nietoperz się budzi.",
+    "Nietoperz budzi się.",
     "Lis ma drugiego gościa."
   ],
   :lesson6 => [
-    "Lekcja 6",
+    "Lekcja szósta",
     "Kot niszczy biurko.",
     "Sowa znajduje bazgroły.",
     "Nietoperz znajduje ołówek.",
     "Lis ma zbyt wielu gości."
   ],
   :lesson7 => [
-    "Ostatnia Lekcja",
-    "Co Ty byś zrobił?"
+    "Ostatnia lekcja",
+    "A co Ty zrobisz?"
   ],
-  :thanks => "Dziękujemy"
+  :thanks => "Podziękowania"
 }
